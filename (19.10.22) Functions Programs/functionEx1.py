19f29cbbdd4c0b690e2c85042c19ddecb6541dbf
bly852 <340926187@gapps.yrdsb.ca>
2019-11-17 21:49:22 -0500

    Update functionEx1.py
    Function documentation update

--- (19.10.22) Functions Programs/functionEx1.py
+++ (19.10.22) Functions Programs/functionEx1.py
@@ -1,4 +1,5 @@
 def palindrome(string):
+    #Takes in a string and outputs a boolean value for whether the string is a palindrome or not
     if str(string) == str(string)[::-1]:
         return True
     else:
