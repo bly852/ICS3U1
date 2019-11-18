9a9a230349c6f4afbbf342b673012beaf08f8fe5
bly852 <340926187@gapps.yrdsb.ca>
2019-11-17 21:40:29 -0500

    Update functionEx2.py
    Finished Knightmove Function

--- (19.10.22) Functions Programs/functionEx2.py
+++ (19.10.22) Functions Programs/functionEx2.py
@@ -1,7 +1,30 @@
 def knightMove(row,col):
-    #Takes the row and column of the knight and returns a tuple of all possible moves
-    columns = ("A", "B", "C", "D", "E", "F", "G", "H")
-    rows = range(1,9)
-    moves = ((2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (-1, -2), (1, -2))
+    #Takes in knight's current position and returns possible moves
     
-    pos = (row, col.capitalize())
+    #Deltas and Column Names
+    columns = ["A", "B", "C", "D", "E", "F", "G", "H"]
+    possibleMoves = [(2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (-1, -2), (1, -2)]
+    
+    newPos = []
+    newPosL = []
+    col = columns.index(col)
+    
+    #Applying deltas to current position
+    for r, c in possibleMoves:
+        moves = row+r, col+c
+        
+        
+        newPos.append(moves)
+    col = columns[col]
+    
+    #Filtering only valid positions in the board
+    newPos = [(row,col) for row, col in newPos if row > 0 and col > 0 and row <= 8 and col <= 8]
+    
+    #Changing interger back to column letter
+    for row, col in newPos:
+        letters = row, columns[col]
+        
+        newPosL.append(letters)
+    return newPosL
+
+print(knightMove(8,"A"))
