9085327bbff7b07f490a23c2944fcaf2077524ef
bly852 <340926187@gapps.yrdsb.ca>
2019-11-17 21:49:55 -0500

    Update slideFunction_ex1.py
    Function documentation update

--- (19.10.22) Functions Programs/slideFunction_ex1.py
+++ (19.10.22) Functions Programs/slideFunction_ex1.py
@@ -1,5 +1,5 @@
 def hyp(a,b):
-    "Function takes A and B and puts them into the Pythagorean Theorem returning C"
+    #Function takes A and B and puts them into the Pythagorean Theorem returning C
     return (a**2 + b**2)**0.5
 
 print(hyp(214,154))
