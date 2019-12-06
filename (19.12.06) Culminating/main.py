cee0743a4167fcd42a7fdfcba379d5c4fb7d749e
bly852 <340926187@gapps.yrdsb.ca>
2019-12-06 09:23:35 -0500

    Create main.py
    file and header creation

---
+++ (19.12.06) Culminating/main.py
@@ -0,0 +1,14 @@
+#         course: ICS3U1 2019
+#       exercise: Culminating Activity
+#           date: 2019-12-06
+# student number: 340926187
+#           name: Brandon Ly
+#    description: pygame game 
+
+# ██████╗ ██╗     
+# ██╔══██╗██║     
+# ██████╔╝██║     
+# ██╔══██╗██║     
+# ██████╔╝███████╗
+# ╚═════╝ ╚══════╝
+
