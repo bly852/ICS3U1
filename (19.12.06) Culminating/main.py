8acb05c547ec3af33ae273442ec75185e3e1fb6b
bly852 <340926187@gapps.yrdsb.ca>
2019-12-10 09:13:57 -0500

    Update main.py
    Added movement ability to both characters
    
    Added a class for food that will randomly spawn

--- (19.12.06) Culminating/main.py
+++ (19.12.06) Culminating/main.py
@@ -12,49 +12,93 @@
 # ██████╔╝███████╗
 # ╚═════╝ ╚══════╝
 
-import pygame, sys, time
+import pygame, sys, time, random
 from pygame.locals import *
 
-#Colours
+# Colours
 blue = (66, 144, 245)
 red = (247, 59, 49)
 green = (62, 247, 49)
 white = (255, 255, 255)
 black = (0, 0, 0)
 
-#Window Dimensions
+# Window Dimensions
 width = 1024
 height = 576
 
-#pygame startup
+# Pygame Startup
 pygame.init()
 fpsClock = pygame.time.Clock()
-screen = pygame.display.set_mode((width,height))
+screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Culminating Assignment")
 
-#Player Class
+def randomColour():
+    r, g, b = random.randint(0,255), random.randint(0,255), random.randint(0,255)
+    colour = r, g, b
+    return colour
+
+# Player Class
 class player:
-    def draw (self, colour, x, y, size):
-        pygame.draw.rect(screen, colour, ((x, y), (size, size)), 0)
+    def __init__(self, colour, x, y, size):
+        self.colour = colour
+        self.x = x
+        self.y = y
+        self.size = size
+        self.vel = 5
+        self.hitbox = ()
     
+    def draw (self, screen):
+        pygame.draw.rect(screen, self.colour, ((self.x, self.y), (self.size, self.size)), 0)
+    
+    
+class food:
+    def __init__(self, randomColour, x, y, size):
+        self.colour = randomColour
+        self.x = x
+        self.y = y
+        self.size = 20
+    
+# players
+player1 = player(green, 40, 100, 50)
+player2 = player(red, 800, 200, 50)
 
-#players
-player1 = player()
-player2 = player()
-
-
-#main loop
+# main loop
 while True:
     screen.fill(white)
     
-    #Exit Game
+    # Exit Game
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                pygame.quit()
+                sys.exit()
     
-    player1.draw(green, 40, 100, 50)
-    player2.draw(blue, 800, 200, 50)
+    # Movement
+    keys = pygame.key.get_pressed()
+    
+    if keys[pygame.K_w] and player1.y > player1.vel:
+        player1.y -= player1.vel
+    if keys[pygame.K_s] and player1.y < height - player1.size - player1.vel:
+        player1.y += player1.vel
+    if keys[pygame.K_a] and player1.x > player1.vel:
+        player1.x -= player1.vel
+    if keys[pygame.K_d] and player1.x < width - player1.size - player1.vel:
+        player1.x += player1.vel
+        
+    if keys[pygame.K_UP] and player2.y > player2.vel:
+        player2.y -= player2.vel
+    if keys[pygame.K_DOWN] and player2.y < height - player2.size - player2.vel:
+        player2.y += player2.vel
+    if keys[pygame.K_LEFT] and player2.x > player2.vel:
+        player2.x -= player2.vel
+    if keys[pygame.K_RIGHT] and player2.x < width - player2.size - player2.vel:
+        player2.x += player2.vel
+                
+    player1.draw(screen)
+    player2.draw(screen)
 
     pygame.display.update()
     fpsClock.tick(60)
