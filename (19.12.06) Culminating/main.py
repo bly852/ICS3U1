ade875429df3687fcafca6935979c2b9bd47924d
bly852 <340926187@gapps.yrdsb.ca>
2019-12-13 09:44:52 -0500

    Update main.py
    Refactoring code into classes

--- (19.12.06) Culminating/main.py
+++ (19.12.06) Culminating/main.py
@@ -5,7 +5,7 @@
 #           name: Brandon Ly
 #    description: Two players (Mr Chun & Mr Pileggi) running around the school collecting food for the food drive.
 
-import pygame, random, os
+import pygame, random, sys
 from pygame.locals import *
 
 # Colours
@@ -14,75 +14,103 @@ red = (247, 59, 49)
 green = (62, 247, 49)
 white = (255, 255, 255)
 black = (0, 0, 0)
+yellow = (255, 255, 0)
+lightgrey = (100, 100, 100)
 
 # Window Dimensions
 width = 1024
 height = 576
 
-# Pygame Startup
-pygame.init()
-fpsClock = pygame.time.Clock()
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Culminating Assignment")
-
-
-def randomColour():
-    r, g, b = random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
-    colour = r, g, b
-    return colour
+tileSize = 32
+gridWidth = width / tileSize
+gridHeight = height / tileSize
 
 # Player Class
 class Player(pygame.sprite.Sprite):
-    def __init__(self, image):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(image).convert()
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((tileSize, tileSize))
+        self.image.fill(green)
         self.rect = self.image.get_rect()
-        self.vel = 5
-        self.rect.center = (width / 2, height / 2)
-
-
-class Food:
-    def __init__(self, randomColour, x, y, size):
-        self.colour = randomColour
         self.x = x
         self.y = y
-        self.size = 20
 
+    def move(self, velX = 0, velY = 0):
+        self.x += velX
+        self.y += velY
 
-# sprite grouping
-all_sprites = pygame.sprite.Group()
+    def update(self):
+        self.rect.x = self.x * tileSize
+        self.rect.y = self.y * tileSize
 
-# players
-player1 = Player('stanley.png')
-all_sprites.add(player1)
+class Wall(pygame.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((tileSize, tileSize))
+        self.image.fill(yellow)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
 
-# main loop
-while True:
-    # Exit Game
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
+# game class
+class Game():
+    def __init__(self):
+        # pygame startup
+        pygame.init()
+        fpsClock = pygame.time.Clock()
+        screen = pygame.display.set_mode((width, height))
+        pygame.display.set_caption("Culminating Assignment")
+
+    def new(self):
+        self.all_sprites = pg.sprite.Group()
+        self.walls = pg.sprite.Group()
+        self.player = Player(self, 10, 10)
+        for x in range(10, 20):
+            Wall(self, x, 5)
+
+    def run(self):
+        self.running = True
+        while self.running:
+            self.events()
+            self.update()
+            self.draw()
+
+    def update(self):
+        self.all_sprites.update()
+
+    def drawGrid(self):
+        for x in range(0, width, tileSize):
+            pygame.draw.line(self.screen, black, (x, 0), (x, height))
+        for y in range(0, height, tileSize):
+            pygame.draw.line(self.screen, black, (0, y), (width, y))
+
+    def draw(self):
+        self.screen.fill(white)
+        self.draw_grid()
+        self.all_sprites.draw(self.screen)
+        pg.display.flip()
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
 
-    # Movement
-    keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_w] and player1.y > player1.vel:
-        player1.y -= player1.vel
-    if keys[pygame.K_s] and player1.y < height - player1.size - player1.vel:
-        player1.y += player1.vel
-    if keys[pygame.K_a] and player1.x > player1.vel:
-        player1.x -= player1.vel
-    if keys[pygame.K_d] and player1.x < width - player1.size - player1.vel:
-        player1.x += player1.vel
+
+
+while True:
+    fpsClock.tick(60)
 
     all_sprites.update()
 
     screen.fill(white)
     all_sprites.draw(screen)
-
-    fpsClock.tick(60)
